b967b99c43a93769735575e71a798b191fc197a1
Nati_Gooday <natnael@gooday.io>
2024-07-05 14:41:29 +0300

    added validation logic using yup

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -10,8 +10,50 @@ import { Input } from "./ui/input";
 import { Button } from "./ui/button";
 import { BeatLoader } from "react-spinners";
 import Error from "./Error";
+import { useState } from "react";
+import * as Yup from "yup";
 
 const Login = () => {
+  const [error, setError] = useState([]);
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+
+    setFormData((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+
+  const handleLogin = async () => {
+    setError([]);
+    try {
+      const schema = Yup.object().shape({
+        email: Yup.string()
+          .email("Invalid Email")
+          .required("Email is Required"),
+        password: Yup.string()
+          .min(6, "Password must be at least 6 charactes")
+          .required("Password is Required"),
+      });
+
+      await schema.validate(formData, { abortEarly: false });
+      // api call
+    } catch (e) {
+      const newErrors = {};
+
+      e?.inner?.forEach((err) => {
+        newErrors[err.path] = err.message;
+      });
+
+      setError(newErrors);
+    }
+  };
+
   return (
     <Card>
       <CardHeader>
@@ -23,8 +65,13 @@ const Login = () => {
       <CardContent className="space-y-2">
         <div className="space-y-1 mb-2">
           <label className="font-bold text-sm">Username:</label>
-          <Input name="email" type="email" placeholder="Enter your email" />
-          <Error message="Please enter user name" />
+          <Input
+            name="email"
+            type="email"
+            placeholder="Enter your email"
+            onChange={handleInputChange}
+          />
+          {error.email && <Error message={error.email} />}
         </div>
         <div className="space-y-1">
           <label className="font-bold text-sm">Password:</label>
@@ -32,13 +79,14 @@ const Login = () => {
             name="password"
             type="password"
             placeholder="Enter your password"
+            onChange={handleInputChange}
           />
-          <Error message="Please enter a password" />
+          {error.password && <Error message={error.password} />}
         </div>
       </CardContent>
       <CardFooter>
-        <Button>
-          {true ? <BeatLoader size={10} color="#fff" /> : "Login"}
+        <Button onClick={handleLogin}>
+          {false ? <BeatLoader size={10} color="#fff" /> : "Login"}
         </Button>
       </CardFooter>
     </Card>
